05cf4e29cbd69a7db2b98fa785be10b63a3bec23
tnembhard <tnembhard@spartaglobal.com>
2019-01-11 11:45:01 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const hello = "Hello me";
+alert (hello);
+console.log("Hi console");
+var myNumb = 90;
+console.log(myNumb);
+var myNumb2 = 110;
+console.log(myNumb2);
+console.log("First Number: ",myNumb," Second Number: ",myNumb2," Equals: ",myNumb+myNumb2);
